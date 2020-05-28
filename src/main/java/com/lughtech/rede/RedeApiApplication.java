7b667a8c82f7199309b5ec8132c33fc19f605424
Luigi <luigitosin@gmail.com>
2020-05-27 22:23:05 -0300

    Criacao do projeto

---
+++ src/main/java/com/lughtech/rede/RedeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lughtech.rede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lughtech/rede/RedeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lughtech.rede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
